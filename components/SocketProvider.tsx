43da487c49b65fb4f5e9d155fe027a7cc0235314
Ayush Singh <ayushpno@gmail.com>
2025-09-30 23:13:20 +0530

    Initial commit

---
+++ components/SocketProvider.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { Socket, io } from 'socket.io-client';
+
+const SocketContext = createContext<Socket | null>(null);
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const newSocket = io('http://localhost:3000');
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
+  return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
+};
